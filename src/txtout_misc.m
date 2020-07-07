1ad9ef453336db2257803ccbefc7b89a23431f72
WataruOhnishi <WataruOhnishi@users.noreply.github.com>
2020-07-07 17:17:41 +0200

    add kaken csv

--- src/txtout_misc.m
+++ src/txtout_misc.m
@@ -6,6 +6,7 @@ if ~isfield(option,'num'), option.num = false; end
 if ~isfield(option,'inJP'), option.inJP = false; end
 if ~isfield(option,'lang'), option.lang = 'jp'; end
 if ~isfield(option,'outOp'), option.outOp = 'all'; end
+if ~isfield(option,'dateExtract'), option.dateExtract = false; end
 
 misc = loadpaper('misc.csv');
 % print 'all' or 'accepted' or 'submitted'
@@ -14,6 +15,11 @@ elseif strcmp(option.outOp,'published')
     misc = misc((misc.Review == '0')|(misc.Review == '1'),:);
 elseif strcmp(option.outOp,'accepted')
     misc = misc((misc.Review == '0')|(misc.Review == '1')|(misc.Review == 'accepted'),:);
+end
+
+if option.dateExtract
+    idx = option.dateFrom <= misc.Year & misc.Year <= option.dateTo;
+    misc = misc(idx,:);
 end
 
 % sort by date
@@ -66,3 +72,24 @@ misc_rgate = misc_rgate(:,{misc_rgate.Properties.VariableNames{1:22}});
 writetable(misc_rgate,'misc_researchmap.csv');
 [~, ~, ~] = movefile('misc_researchmap.csv',['./publications/',datestr(datetime('now'),'yyyymmdd')],'f');
 
+% kaken csv
+conf_kaken = conf;
+conf_kaken = addvars(conf_kaken,conf_kaken.Year(:).Year);
+conf_kaken = addvars(conf_kaken,double(conf_kaken.Type == '4'));
+conf_kaken = conf_kaken(:,[4,2,6,25,25,13,26]);
+conf_kaken.Properties.VariableNames{'Author_EN'} = 'Author';
+conf_kaken.Properties.VariableNames{'Title_EN'} = 'Title';
+conf_kaken.Properties.VariableNames{'Journal_EN'} = 'Journal';
+
+domconf_kaken = domconf;
+domconf_kaken = addvars(domconf_kaken,domconf_kaken.Year(:).Year);
+domconf_kaken = addvars(domconf_kaken,double(domconf_kaken.Type == '4'));
+domconf_kaken = domconf_kaken(:,[3,1,5,25,25,13,26]);
+domconf_kaken.Properties.VariableNames{'Author_JP'} = 'Author';
+domconf_kaken.Properties.VariableNames{'Title_JP'} = 'Title';
+domconf_kaken.Properties.VariableNames{'Journal_JP'} = 'Journal';
+writetable(vertcat(conf_kaken,domconf_kaken),'conf_kaken.csv','WriteVariableNames',false,'Encoding','Shift-JIS');
+[~, ~, ~] = movefile('conf_kaken.csv',['./publications/',datestr(datetime('now'),'yyyymmdd')],'f');
+
+
+end
